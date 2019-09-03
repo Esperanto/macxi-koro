7cbadb24ea4df2fbd8536dc636a41651609439fa
Paul Würtz <paulwuertz@posteo.de>
2019-09-04 01:21:38 +0200

    baza ludo sen bildoj

---
+++ README.md
@@ -0,0 +1,10 @@
+## Maĉi Koro
+
+Traduko de [Machi Koro](https://www.kosmos.de/spielware/spiele/familienspiele/7444/machi-koro)
+
+## Generado
+
+Uzu `python generate.py`. Vi bezonas krome:
+
+* imagemagic, aka `convert`
+  * mi havis [jena](https://stackoverflow.com/questions/52998331/imagemagick-security-policy-pdf-blocking-conversion) problemo, kun permesojn, sed facile adapteblas
---
+++ generate.py
@@ -0,0 +1,19 @@
+import jinja2, yaml, os
+import subprocess, textwrap
+
+kartoj = yaml.safe_load(open("kartoj.yaml").read())   # listo de kartoj
+
+for k in kartoj: #trancxu tekston al sammlongaj lineoj
+    k["teksto"] = textwrap.wrap(k["teksto"], 27)
+    k["svg"] = k["bildo"] if os.path.isfile(k["bildo"]) else "akvobotelo.svg"
+kartoj = [k for k in kartoj] #for i in range(k["kvanto"])] # adpatas la liston por kvanto de karto
+pagxoj = [kartoj[i:i+9] for i in range(0, len(kartoj), 9)] #disigas la kartaro en pagxojn po de 9 kartoj
+
+for i, pagxo in enumerate(pagxoj):
+    template_str = open("templates/sxablono.svg.jinja2").read()
+    t = jinja2.Template(template_str)
+    kartoj_offsets = zip(pagxo, [(0,0), (61,0), (122,0), (0,91), (61,91), (122,91), (0,182), (61,182), (122,182)])
+    r = t.render(kartoj_offsets=kartoj_offsets)
+    open('svg/{}.svg'.format(i), "w").write(r)
+    subprocess.check_output(['inkscape','-z', '--export-dpi', '300', 'svg/{}.svg'.format(i), '-e', 'img/{}.png'.format(i)])
+subprocess.check_output(['convert', 'img/[0-9].png', 'ludo.pdf'])
